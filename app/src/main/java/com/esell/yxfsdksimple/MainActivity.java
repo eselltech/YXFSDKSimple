3c6451e59ad8b38e44f9399c6bf606fe0a185f44
niulei <niulei@eselltech.com>
2019-11-25 19:38:09 +0800

    1、动态权限申请 2、版本更新

--- app/src/main/java/com/esell/yxfsdksimple/MainActivity.java
+++ app/src/main/java/com/esell/yxfsdksimple/MainActivity.java
@@ -1,10 +1,16 @@
 package com.esell.yxfsdksimple;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.esell.component_common.model.AD;
 import com.esell.component_common.model.OnAdListener;
@@ -24,13 +30,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {/*6.0以上申请权限*/
+            if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) || PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "未获取到存储权限", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE}, 101);
+                return;
+            }
+        }
+        init();
+
+    }
+
+    private void init() {
         /*获取实例*/
         yxf = Yxf.getInstance();
         /*打开调试信息*/
         yxf.debug(true);
         /*初始化*/
-        yxf.init(getApplicationContext(), " yxfAppId", "yxfAppId",
-                new OnInitListener() {
+        yxf.init(getApplicationContext(), " yxfAppId", "yxfAppId", new OnInitListener() {
             @Override
             public void onSuccess() {
                 Log.i(TAG, "onSuccess (line 20): ");
@@ -41,16 +62,16 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "onFailed (line 26): code : " + code + ",msg : " + msg);
             }
         });
-//        自实现配置
-//        ConfigManager configManager = ConfigManager.getInstance();
-//        本地缓存
-//        configManager.setCache();
-//        图片加载
-//        configManager.setImageLoad();
-//        网络请求
-//        configManager.setNetRequest();
-//        下载
-//        configManager.setDownload();
+        //        自实现配置
+        //        ConfigManager configManager = ConfigManager.getInstance();
+        //        本地缓存
+        //        configManager.setCache();
+        //        图片加载
+        //        configManager.setImageLoad();
+        //        网络请求
+        //        configManager.setNetRequest();
+        //        下载
+        //        configManager.setDownload();
 
         final SlotView slotView = new SlotView(getApplicationContext());
         /*配置屏效宝广告*/
@@ -72,10 +93,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         /*主动获取rtb广告*/
-        RtbRequest.request(this, "appId", "appKey", "设备唯一编码", "广告位id", "类型", 1/*数量*/, new OnAdListener() {
+        RtbRequest.request(this, "appId", "appKey", "设备唯一编码", "广告位id", "类型", 1/*数量*/,
+                new OnAdListener() {
             @Override
             public void onAd(List<? extends AD> adList) {
-                Log.d(TAG,  "onAd (line 21): "+adList);
+                Log.d(TAG, "onAd (line 21): " + adList);
             }
         });
     }
@@ -84,5 +106,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         yxf.destroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) && PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            init();
+        }
     }
 }
