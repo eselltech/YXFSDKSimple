5b0b21c2eda00471db00205d00fb1eb13c19e01a
niulei <niulei@eselltech.com>
2020-07-15 11:08:26 +0800

    1、版本更新

--- app/src/main/java/com/esell/yxfsdksimple/MainActivity.java
+++ app/src/main/java/com/esell/yxfsdksimple/MainActivity.java
@@ -8,25 +8,43 @@ import android.support.annotation.NonNull;
 import android.support.annotation.RequiresPermission;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.esell.component_common.YLog;
 import com.esell.component_common.model.AD;
-import com.esell.component_common.model.OnAdListener;
+import com.esell.yxf.CustomAD;
+import com.esell.yxf.CustomADSet;
 import com.esell.yxf.OnInitListener;
 import com.esell.yxf.SlotView;
 import com.esell.yxf.Yxf;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = com.esell.yxfsdksimple.MainActivity.class.getSimpleName();
     private Yxf yxf;
+    /**
+     * 测试图片广告素材
+     */
+    String urls[] = {"https://file1.yixinfa" + ".cn/workstation/2007/15" +
+            "/285646a7ef0b64c738bd3823379bd45a.jpg",
+            "https://file1" + ".yixinfa.cn/workstation" + "/2007/15" +
+                    "/cdb5ab994a02700cdc87024332f828a6.png",
+            "https" + "://file1.yixinfa" + ".cn" + "/workstation/2007/15" +
+                    "/a757b4ae733db6a488cb7ed2674f9496.jpg",
+            "https://file1" + ".yixinfa" + ".cn" + "/workstation/2007/15" +
+                    "/da19d792aaed9fdd398b746c311cb32d.jpg"};
+    /**
+     * TODO: 设置参数
+     */
+    public static final String APP_ID = ;
+    public static final String APP_KEY = ;
+    public static final int SLOT_ID = ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +56,16 @@ public class MainActivity extends AppCompatActivity {
                     Manifest.permission.READ_EXTERNAL_STORAGE) || PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) || PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) || PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
                 Toast.makeText(this, "未获取到所需权限", Toast.LENGTH_SHORT).show();
                 ActivityCompat.requestPermissions(this,
-                        new String[]{
-                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
                                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                                 Manifest.permission.ACCESS_FINE_LOCATION,
-                                Manifest.permission.ACCESS_COARSE_LOCATION
-                        }, 101);
+                                Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
                 return;
             }
         }
         init();
-
     }
+
     @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
     private void init() {
         /*获取实例*/
@@ -57,22 +73,26 @@ public class MainActivity extends AppCompatActivity {
         /*打开调试信息*/
         yxf.debug(true);
         /*报备广告位id*/
-        yxf.report(slotId1, slotId2, slotId3);
+        yxf.report(SLOT_ID);
         /*初始化*/
-        yxf.init(getApplicationContext(), " yxfAppId", "yxfAppId", new OnInitListener() {
+        yxf.init(getApplicationContext(), APP_ID, APP_KEY, new OnInitListener() {
             @Override
             public void onSuccess() {
                 /*获取设备编号*/
                 String deviceNum = yxf.getDeviceNum();
-                Log.i(TAG, "onSuccess (line 20): " + deviceNum);
+                YLog.i("onSuccess (line 20): " + deviceNum);
             }
 
             @Override
             public void onFailed(int code, String msg) {
-                Log.e(TAG, "onFailed (line 26): code : " + code + ",msg : " + msg);
+                YLog.e("onFailed (line 26): code : " + code + ",msg : " + msg);
             }
         });
+        //广告位实例方式2  不需要link
+        SlotView slotView = yxf.newSlotView(SLOT_ID, "广告位描述");
 
+        FrameLayout container = findViewById(R.id.container);
+        container.addView(slotView);
         //        自实现配置
         //        ConfigManager configManager = ConfigManager.getInstance();
         //        本地缓存
@@ -83,17 +103,14 @@ public class MainActivity extends AppCompatActivity {
         //        configManager.setNetRequest();
         //        下载
         //        configManager.setDownload();
-        //广告位实例方式1
-        final SlotView slotView = new SlotView(getApplicationContext());
-        /*设置广告位id*/
-        slotView.setYxfSlotId(slotId);
-        /*添加广告位描述*/
-        slotView.setYxfSlotDes("广告位描述");
-        yxf.link(slotView);
-        //广告位实例方式2  不需要link
-        //         SlotView slotView = yxf.newSlotView(slotId, "广告位描述");
-        FrameLayout container = findViewById(R.id.container);
-        container.addView(slotView);
+        //        //广告位实例方式1
+        //        final SlotView slotView = new SlotView(getApplicationContext());
+        //        /*设置广告位id*/
+        //        slotView.setYxfSlotId(slotId);
+        //        /*添加广告位描述*/
+        //        slotView.setYxfSlotDes("广告位描述");
+        //        yxf.link(slotView);
+
 
         //        打开调试信息
         //        yxf.debug(true);
@@ -122,16 +139,6 @@ public class MainActivity extends AppCompatActivity {
         //        yxf.reportDownloadStatus(ad);
     }
 
-    public void btnClick(View view) {
-        /*主动获取普通广告*/
-        yxf.getNormalAdList(new OnAdListener() {
-            @Override
-            public void onAd(List<? extends AD> adList) {
-
-            }
-        });
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -146,5 +153,31 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE) && PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             init();
         }
+    }
+
+    public void getDeviceNum(View view) {
+        YLog.d("deviceNum : " + yxf.getDeviceNum());
+    }
+
+    public void getNormalAdList(View view) {
+        yxf.getNormalAdList(adList -> YLog.d(adList == null ? "null" : adList.toString()));
+    }
+
+    public void addCustomList(View view) {
+        List<CustomAD> list = new ArrayList<>();
+        for (String url : urls) {
+            CustomAD customAD = new CustomAD(url.hashCode(), SLOT_ID, AD.ContentType.IMG, url,
+                    15 * 1000);
+            list.add(customAD);
+        }
+        CustomADSet customADSet = yxf.getCustomADSet();
+        customADSet.addCustomList(list);
+        YLog.d("customADSet.size : " + customADSet.size());
+    }
+
+    public void cleanCustomList(View view) {
+        CustomADSet customADSet = yxf.getCustomADSet();
+        customADSet.clear();
+        YLog.d("customADSet.size : " + customADSet.size());
     }
 }
