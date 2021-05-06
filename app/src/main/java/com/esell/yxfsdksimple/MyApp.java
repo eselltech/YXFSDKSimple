83c3af10d8decd0a13d2eca3d3781d44f03ce651
niulei@eselltech.com <niulei@eselltech.com>
2021-05-06 10:00:53 +0800

    1、去除WorkManager IllegalStateException 提示

--- app/src/main/java/com/esell/yxfsdksimple/MyApp.java
+++ app/src/main/java/com/esell/yxfsdksimple/MyApp.java
@@ -5,18 +5,25 @@ import android.content.Context;
 
 import com.esell.yxf.Yxf;
 
+import androidx.annotation.NonNull;
 import androidx.multidex.MultiDex;
+import androidx.work.Configuration;
 
 /**
- *
  * @author NiuLei
  * @date 2019/10/22 16:28
  */
-public class MyApp extends Application {
+public class MyApp extends Application implements Configuration.Provider {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
         Yxf.getInstance().attachBaseContext(base);
+    }
+
+    @NonNull
+    @Override
+    public Configuration getWorkManagerConfiguration() {
+        return new Configuration.Builder().setMinimumLoggingLevel(android.util.Log.INFO).build();
     }
 }
